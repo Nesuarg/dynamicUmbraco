4afadb78a8058ebe54f1187ceec727ce193ff662
jannik-winpc <jgrausen@gmail.com>
2015-10-28 18:13:05 +0100

    moving site to github

---
+++ WebdeveloperCoperation/WebdeveloperCoperation/Controllers/contactFormSurfaceController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebdeveloperCoperation.viewModel;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+
+namespace WebdeveloperCoperation.Controllers
+{
+    public class contactFormSurfaceController : SurfaceController
+    {
+      public ActionResult Index()
+      {
+          return PartialView("contactForm", new contactForm());
+      }
+        [HttpPost]
+      public ActionResult HandleFormSubmit(contactForm model)
+      {
+          if (!ModelState.IsValid) { return CurrentUmbracoPage(); }
+ 
+          IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "comment");
+
+          comment.SetValue("name", model.Name);
+          comment.SetValue("email", model.Email);
+          comment.SetValue("subject", model.Subject);
+          comment.SetValue("message", model.Message);
+
+          
+          // For saving only
+          Services.ContentService.Save(comment);
+          TempData["success"] = true;
+          // For saving and publishing
+          //Services.ContentService.SaveAndPublishWithStatus(comment);
+          
+          
+            
+            return RedirectToCurrentUmbracoPage();
+
+          
+      }
+    }
+
+}
+
---
+++ WebdeveloperCoperation/WebdeveloperCoperation/viewModel/contactForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebdeveloperCoperation.viewModel
+{
+    public class contactForm
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage="this it not a valid emailadress")]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+
+    }
+}
